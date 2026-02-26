243707e2a9fca9d00d8477cce710cdff33364693
Aluizio Antônio <neto.yuko197@gmail.com>
2026-02-26 14:04:06 -0300

    adiciona tela de cadastro, ajusta link da Home, ajusta App para controle de telas

--- frontend-precificacao/src/App.jsx
+++ frontend-precificacao/src/App.jsx
@@ -1,23 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroProduto from './pages/CadastroProduto';
+
 export default function App() {
   return (
-    <main className="min-h-screen bg-[#0d1117] flex flex-col items-center justify-center p-6">
-      {/* Titulo*/}
-      <header className="mb-10 text-center">
-        <h1 className="text-xl font-bold text-white">
-          Sistema de Precificação
-        </h1>
-      </header>
-      {/* Container dos botões*/}
-      <nav className="flex flex-col w-full max-w-2xs gap-4">
-        <button className="flex items-center justify-center gap-3 bg-[#1a1d24] border border-gray-700 rounded-2xl p-5 w-full active:scale-95 transition-transform">
-          <span className="text-emerald-500 font-bold text-xl">#</span>
-          <span className="text-white font-semibold text-lg">Novo Produto</span>
-        </button>
-        <button className="flex items-center justify-center gap-3 bg-[#1a1d24] border border-gray-700 rounded-2xl p-5 w-full active:scale-95 transition-transform">
-          <span className="text-emerald-500 font-bold text-xl">#</span>
-          <span className="text-white font-semibold text-lg">Ver Produtos</span>
-        </button>
-      </nav>
-    </main>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Quando a URL for "/", mostra a Home */}
+        <Route path="/" element={<Home />} />
+        
+        {/* Quando a URL for "/cadastro", mostra a tela de Cadastro */}
+        <Route path="/cadastro" element={<CadastroProduto />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
