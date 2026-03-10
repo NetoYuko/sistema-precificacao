d99a0d6d6a220adbd01d67739f4a1b3714194fbf
Aluizio Antônio <neto.yuko197@gmail.com>
2026-03-10 15:21:31 -0300

    instala e configura cors

--- backend-precificacao/index.js
+++ backend-precificacao/index.js
@@ -15,8 +15,8 @@ const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
 
 // Configuração de Middlewares
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const pool = new Pool ({
     connectionString: process.env.SERVER_PORT,
